90cbc192f102a2901c61d9b1fdaf4bf210336d86
Jonathandjuarez7 <126812482+Jonathandjuarez7@users.noreply.github.com>
2023-03-03 14:08:27 -0300

    Add files via upload

---
+++ JS/eventos.js
@@ -0,0 +1,8 @@
+document.getElementById("boton").onclick=function(){
+    document.getElementById("boton").innerHTML="Profesor Raúl Sevilla ,facultad ciencias politicas y sociales: A quien pueda interesar,Esteban Lopez, mayor de edad, declaro que durante los años de trato,ha demostrado ser un trabajador responsable, honesto, amable y, lo más importante, eficiente."
+
+}
+
+document.getElementById("boton2").onclick=function(){
+    document.getElementById("boton2").innerHTML="Curso online de Administración de empresas finalizado por Esteban Lopez inpartido por la universidad UNITEC"
+}
